36056e2823ca28a21852a60397f0683d8036cc10
Yousif Faraj <yfaraj96@gmail.com>
2020-10-01 21:10:07 -0400

    Star system updated

--- Client/Components/AddToCart/subComponents/productInfo/productInfo.jsx
+++ Client/Components/AddToCart/subComponents/productInfo/productInfo.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import './productInfo.css';
+import Rating from 'react-rating';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as fiStar} from '@fortawesome/free-regular-svg-icons'
+
 
 
 var ProductInfo = ({storeReviews, store, sales, title, price}) => {
-  console.log(storeReviews)
+  console.log(storeReviews);
   return (
     <div>
       <h4>{store}</h4>
       <p className="storeTotals">
-        {sales} sales <span className="divider">|</span> *****
+        {sales} sales <span className="divider">|</span>
+        <Rating
+          initialRating={storeReviews}
+          emptySymbol = {<FontAwesomeIcon icon={fiStar} />}
+          fullSymbol = {<FontAwesomeIcon icon={faStar} />}
+          readonly
+        />
       </p>
       <h6>{title}</h6>
       <p className="price">
