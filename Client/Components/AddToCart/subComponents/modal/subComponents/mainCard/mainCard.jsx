95434f00b16559691001fdac5eadb0906fae4c0d
Yousif Faraj <yfaraj96@gmail.com>
2020-10-03 16:45:27 -0400

    modal css updated

--- Client/Components/AddToCart/subComponents/modal/subComponents/mainCard/mainCard.jsx
+++ Client/Components/AddToCart/subComponents/modal/subComponents/mainCard/mainCard.jsx
@@ -7,12 +7,12 @@ const MainCard = ({title, price, image}) => {
       <div className="mainCard">
         <img src={image} />
         <div className="mainInfo">
-          <h3>{title}</h3>
+          <h4>{title}</h4>
           <h4>{price}</h4>
           <button>Go to cart</button>
         </div>
       </div>
-      <div className="loading"></div>
+      <div className="loading"><span className="innerLoading"></span></div>
       <h2 className="modalDeal">You are $15.00 away from getting free shipping from this store</h2>
     </div>
   )
